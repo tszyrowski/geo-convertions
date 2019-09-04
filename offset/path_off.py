a8fe750e6c38682064222ba73ce4c36178b4cb01
Szyrowski, Tomasz <tomasz.szyrowski@metoffice.gov.uk>
2019-09-04 16:46:50 +0100

    further tries

--- offset/path_off.py
+++ offset/path_off.py
@@ -4,6 +4,7 @@ from bokeh.plotting import figure
 from bokeh.io.showing import show
 from shapely.geometry.point import Point
 from shapely.geometry.linestring import LineString
+from shapely.geometry.multilinestring import MultiLineString
 
 def create_input_points():
     """showing parallel lne to closed line"""
@@ -31,12 +32,16 @@ def make_points():
     x0 = np.arange(0, np.pi*2, 0.5)
     y1 = np.sin(x0)
     y2 = np.sin(x0)*(-1)
-#     y2[-1]=y1[-1]
-    print(len(x0), len(y1), len(y2))
+    
+    # lines crossing each other
     xx = np.concatenate([x0, x0[::-1]])
     yy = np.concatenate([y1, y2])
+    
+    # simple line
+#     xx = x0
+#     yy = y1
+    
     points = [Point(x, y) for (x, y) in zip(xx, yy)]
-    print(points)
     return points
     
 
@@ -58,15 +63,30 @@ def plot_line(line_ob, fig=None):
     """Function takes shapely.linestring extract coordinates and plot on a figure."""
 
     if not fig:
-        fig = figure(title="linestring plot")    
-    x, y = line_ob.xy
-    x = x.tolist()
-    y = y.tolist()
-    print('x=', x,'\ny=', y)
-    fig.line(x, y, color='blue')
+        fig = figure(title="linestring plot")  
+    cmp = ['blue', 'red', 'green', 'gray', 'yellow']  
+    col = 0
+    def _plot_line(ob, fig):
+        x, y = ob.xy
+        x = x.tolist()
+        y = y.tolist()
+        print('x=', x,'\ny=', y)
+        fig.line(x, y, color=cmp[col])
+    if type(line_ob) is LineString:
+        _plot_line(line_ob, fig)
+    elif type(line_ob) is MultiLineString:
+        for ob in line_ob:
+            _plot_line(ob, fig)
     return fig 
-            
 
+
+def shift_points(points, distance = 1, side='left', resolution=16, join_style=3, mitre_limit=5.0):
+    """Takes array of points, create linestring, shift the linestring and return shifted"""
+    line = LineString(points)
+    shift_line = line.parallel_offset(distance, side)
+    print('shift_line=\n', shift_line)
+    return shift_line
+    
 def main_vis(fig):
     show(fig)
 
@@ -74,5 +94,6 @@ if __name__ == '__main__':
     points = make_points()
 #     fig = create_input_points()
     fig = plot_points(points)#, fig=fig)
-    fig = plot_line(LineString(points), fig)
+#     fig = plot_line(LineString(points), fig)
+    fig = plot_line(shift_points(points), fig)
     main_vis(fig)
